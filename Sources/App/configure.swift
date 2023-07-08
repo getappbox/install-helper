b098c83a73c1e13c8c936dca4bc761b8a8d94b1a
Vineet Choudhary <vineetchoudhary@live.in>
2023-07-08 19:41:39 +0530

    Set custom port

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,6 +5,10 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+	if let port = Int(Environment.get("PORT") ?? "2550") {
+		app.http.server.configuration.port = port
+	}
+
     // register routes
     try routes(app)
 }
