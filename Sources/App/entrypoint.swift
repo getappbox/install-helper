d480d00c055124574aa9115e16e3cbdbb16ab178
Vineet Choudhary <vineetchoudhary@live.in>
2025-08-23 16:56:18 +0530

    Use vapor base execution queue for swift 5

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Dispatch
 import Logging
 
 @main
@@ -23,11 +24,29 @@ enum Entrypoint {
         defer { app.shutdown() }
         do {
             try await configure(app)
-            try app.run()
+            try await app.runFromAsyncMainEntrypoint()
         } catch {
             app.logger.report(error: error)
             throw error
         }
         #endif
+    }
+}
+
+/// This extension is temporary and can be removed once Vapor gets this support.
+private extension Vapor.Application {
+    static let baseExecutionQueue = DispatchQueue(label: "vapor.codes.entrypoint")
+    
+    func runFromAsyncMainEntrypoint() async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            Vapor.Application.baseExecutionQueue.async { [self] in
+                do {
+                    try self.run()
+                    continuation.resume()
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
     }
 }
