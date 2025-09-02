17951e1b5136d9ef2db87ba958643f64ab1f5f24
Vineet Choudhary <vineetchoudhary@live.in>
2025-09-02 10:53:00 +0530

    Modify IPA file URL directly download from dropbox

--- Sources/App/Controllers/InstallController.swift
+++ Sources/App/Controllers/InstallController.swift
@@ -15,8 +15,9 @@ struct InstallController: RouteCollection {
 
 	func installManifest(req: Request) async throws -> ClientResponse {
 		let pathComponents = req.url.path.components(separatedBy: "/")
-		var manifestURLString = "https://dl.dropboxusercontent.com"
+		var path = ""
 		var manifestURLQueryItems = [URLQueryItem]()
+		manifestURLQueryItems.append(.init(name: "dl", value: "1"))
 		for pathComponent in pathComponents {
 			if pathComponent.isEmpty || pathComponent == "install" {
 				continue
@@ -27,18 +28,103 @@ struct InstallController: RouteCollection {
 				}
 				manifestURLQueryItems.append(.init(name: queryParam[0], value: queryParam[1]))
 			} else {
-				manifestURLString.append("/\(pathComponent)")
+				path.append("/\(pathComponent)")
 			}
 		}
-		var manifestURLComponents = URLComponents(string: manifestURLString)
-		if !manifestURLString.isEmpty {
-			manifestURLComponents?.queryItems = manifestURLQueryItems
-		}
+		var manifestURLComponents = URLComponents()
+		manifestURLComponents.scheme = "https"
+		manifestURLComponents.host = "www.dropbox.com"
+		manifestURLComponents.path = path
+		manifestURLComponents.queryItems = manifestURLQueryItems
 
-		guard let manifestURL = manifestURLComponents?.url else {
+		guard let manifestURLString = manifestURLComponents.string else {
 			throw Abort(.badRequest, reason: "Invalid URL.")
 		}
 
-		return try await req.client.get(.init(stringLiteral: manifestURL.absoluteString))
+		var manifestResponse = try await req.client.get(.init(stringLiteral: manifestURLString))
+
+		// ensure we got a valid response with body
+		guard manifestResponse.status == .ok, let body = manifestResponse.body else {
+			return manifestResponse
+		}
+
+		// modify manifest body to update asset URL
+		guard let updatedBody = updatedManifestBody(from: body, logger: req.logger) else {
+			throw Abort(
+				.internalServerError,
+				headers: manifestResponse.headers,
+				reason: "Failed to update manifest.",
+				identifier: manifestURLString)
+		}
+
+		manifestResponse.body = updatedBody
+		manifestResponse.headers.replaceOrAdd(name: .contentLength, value: "\(updatedBody.readableBytes)")
+		manifestResponse.headers.replaceOrAdd(name: .contentType, value: "application/xml")
+		return manifestResponse
+	}
+
+	/// Update manifest body to modify asset URL to ensure direct download from Dropbox
+	private func updatedManifestBody(from buffer: ByteBuffer, logger: Logger) -> ByteBuffer? {
+		guard let data = buffer.getData(at: 0, length: buffer.readableBytes) else {
+			return nil
+		}
+
+		do {
+			var format = PropertyListSerialization.PropertyListFormat.xml
+			guard var plist = try PropertyListSerialization.propertyList(
+				from: data,
+				options: .mutableContainersAndLeaves,
+				format: &format) as? [String: Any] else {
+				return nil
+			}
+
+			guard var items = plist["items"] as? [[String: Any]],
+				  !items.isEmpty,
+				  var firstItem = items[0] as? [String: Any],
+				  var assets = firstItem["assets"] as? [[String: Any]],
+				  !assets.isEmpty,
+				  var firstAsset = assets[0] as? [String: Any],
+				  let urlString = firstAsset["url"] as? String,
+				  var urlComponents = URLComponents(string: urlString) else {
+				return nil
+			}
+
+			// Update host if needed
+			if urlComponents.host == "dl.dropboxusercontent.com" {
+				urlComponents.host = "www.dropbox.com"
+			}
+
+			// ensure dl=1 is present to force direct download from Dropbox
+			var queryItems = urlComponents.queryItems ?? []
+			let dlQueryItem = URLQueryItem(name: "dl", value: "1")
+			if let dlQueryItemIndex = queryItems.firstIndex(where: { $0.name == dlQueryItem.name }) {
+				queryItems[dlQueryItemIndex] = dlQueryItem
+			} else {
+				queryItems.append(dlQueryItem)
+			}
+			urlComponents.queryItems = queryItems
+
+			if let newURL = urlComponents.string {
+				firstAsset["url"] = newURL
+				assets[0] = firstAsset
+				firstItem["assets"] = assets
+				items[0] = firstItem
+				plist["items"] = items
+			} else {
+				return nil
+			}
+
+			let newData = try PropertyListSerialization.data(
+				fromPropertyList: plist,
+				format: .xml,
+				options: 0
+			)
+			var newBuffer = ByteBufferAllocator().buffer(capacity: newData.count)
+			newBuffer.writeBytes(newData)
+			return newBuffer
+		} catch {
+			logger.error("Manifest plist modification failed: \(error.localizedDescription)")
+			return nil
+		}
 	}
 }
